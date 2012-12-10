99a8b460108b15652a73fcf599928fe7461d9ec3
James Mastros <james@mastros.biz>
2012-12-10 22:52:50 +0000

    Less callbacking when we are asked for methods on Object Also do some pre-querying of Method

--- src/uk/me/desert_island/theorbtwo/bridge/InterfaceImplementor.java
+++ src/uk/me/desert_island/theorbtwo/bridge/InterfaceImplementor.java
@@ -12,7 +12,7 @@ public class InterfaceImplementor {
     {
         Class iface = Class.forName(interface_name);
         ClassLoader loader = ClassLoader.getSystemClassLoader();
-        InvocationHandler handler = new MyInvocationHandler(callback);
+        InvocationHandler handler = new MyInvocationHandler(callback, interface_name);
         // There's *got* to be a better way of doing this.
         Class[] ifaces = new Class[1];
         ifaces[0]=iface;
@@ -22,21 +22,41 @@ public class InterfaceImplementor {
     
     public static class MyInvocationHandler implements InvocationHandler {
         private PassthroughRunnable callback;
+        private String interface_name;
 
-        public MyInvocationHandler(PassthroughRunnable a_callback) {
+        public MyInvocationHandler(PassthroughRunnable a_callback, String a_interface_name) {
             callback = a_callback;
+            interface_name = a_interface_name;
         }
         
-        public Object invoke(Object proxy, Method method, Object[] args) {
+        public Object invoke(Object proxy, Method method, Object[] args) 
+            throws Throwable
+        {
             System.err.println("Invoke, method (long) name: "+method.toGenericString());
             // FIXME: Return?
-            /*
-            if(method.getName().equals("toString")) {
-                // Less junk over the wire please!
-                return "MyInvocationHandler (magic goes here)";
+            
+            String decl_class = method.getDeclaringClass().getCanonicalName();
+            String short_name = method.getName();
+            String long_name = method.toGenericString();
+
+            Object[] method_info = new Object[4];
+            method_info[0] = method;
+            method_info[1] = decl_class;
+            method_info[2] = short_name;
+            method_info[3] = long_name;
+
+            if(decl_class.equals("java.lang.Object")) {
+                if (short_name.equals("toString")) {
+                    return "InterfaceImplementor["+interface_name+"]";
+                } else {
+                    // Just method.getName().equals("toString") ?
+                    // Less junk over the wire please!
+                    return method.invoke(proxy, args);
+                }
             }
-            */
-            callback.run_extended(this, proxy, method, args);
+            
+            // These will always return null immediately / ignore returns, do we care?
+            callback.run_extended(this, proxy, method_info, args);
             return null;
         }
     }

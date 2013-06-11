59a7299f195033cbbeea12e22f97f47d4d2313fe
Jess Robinson <castaway@desert-island.me.uk>
2013-06-11 17:23:03 +0100

    Attempt to wait for Constructor timeout for 1 second

--- src/uk/me/desert_island/theorbtwo/bridge/ConstructorCallRunnable.java
+++ src/uk/me/desert_island/theorbtwo/bridge/ConstructorCallRunnable.java
@@ -3,6 +3,9 @@ package uk.me.desert_island.theorbtwo.bridge;
 import java.lang.Runnable;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
 
 public class ConstructorCallRunnable implements Runnable {
     private Class klass;
@@ -10,6 +13,9 @@ public class ConstructorCallRunnable implements Runnable {
     private Constructor constr;
 
     public volatile Object constructed = null;
+    public volatile Exception exception = null;
+
+    public Semaphore finished = new Semaphore(0);
     
     public ConstructorCallRunnable(String perl_class, Object... a_args) 
         throws NoSuchMethodException, ClassNotFoundException
@@ -30,15 +36,27 @@ public class ConstructorCallRunnable implements Runnable {
     {
         try {
             constructed = constr.newInstance(args);
-        } catch (IllegalAccessException e) {
-            // HELP ME, WHAT CAN I DO HERE?
-        } catch (InvocationTargetException e) {
-            // HELP ME, WHAT CAN I DO HERE?
-        } catch (InstantiationException e) {
+        } catch (Exception e) {
+            exception = e;
         }
+        finished.release();
     }
 
-    public Object getResult() {
+    public Object getResult()
+        throws Exception
+    {
+        // We need to wait here until run() has actually finished!
+        // finished.acquire();
+        Boolean got_finished = finished.tryAcquire(1, 1, TimeUnit.SECONDS);
+        if (got_finished) {
+            System.err.println("Already finished the ConstructorCallRunnable");
+        } else {
+            System.err.println("ConstructorCallRunnable not yet finished, but continuing anyway");
+        }
+        if (exception != null) {
+            throw exception;
+        }
+
         return constructed;
     }
 }
